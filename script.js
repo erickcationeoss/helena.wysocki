c8434d16a6312fda64d8a17afccbde48d3ff9da9
erickmokado <168188542+erickcationeoss@users.noreply.github.com>
2024-10-18 22:17:32 -0300

    java

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ let currentIndex = 0;
             const offset = -currentIndex * 100; // Move o carrossel
             images.style.transform = `translateX(${offset}%)`;
         }
+
+
+        let currentIndex1 = 0;
+
+        function moveCarouseles(direction) {
+            const images = document.querySelector('.carouseles-images');
+            const totalImages = images.children.length;
+            currentIndex += direction;
+
+            if (currentIndex < 0) {
+                currentIndex = totalImages - 1; // Vai para a última imagem
+            } else if (currentIndex >= totalImages) {
+                currentIndex = 0; // Volta para a primeira imagem
+            }
+
+            const offset = -currentIndex * 100; // Move o carrossel
+            images.style.transform = `translateX(${offset}%)`;
+        }
