660ba6d064147c587f9f3dde98e9391f8f402137
erickmokado <168188542+erickcationeoss@users.noreply.github.com>
2024-10-18 23:07:52 -0300

    banners111

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ let currentIndex = 0;
         function moveCarouseles(direction) {
             const images = document.querySelector('.carouseles-images');
             const totalImages = images.children.length;
-            currentIndex += direction;
+            currentIndex1 += direction;
 
-            if (currentIndex < 0) {
-                currentIndex = totalImages - 1; // Vai para a última imagem
-            } else if (currentIndex >= totalImages) {
-                currentIndex = 0; // Volta para a primeira imagem
+            if (currentIndex1 < 0) {
+                currentIndex1 = totalImages - 1; // Vai para a última imagem
+            } else if (currentIndex1 >= totalImages) {
+                currentIndex1 = 0; // Volta para a primeira imagem
             }
 
-            const offset = -currentIndex * 100; // Move o carrossel
+            const offset = -currentIndex1 * 100; // Move o carrossel
             images.style.transform = `translateX(${offset}%)`;
         }
